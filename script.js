f0dc2ff3440edfb3e84c2987a71dcc260785d8f0
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-09-05 18:34:03 +0300

    step 56

--- script.js
+++ script.js
@@ -139,6 +139,11 @@ class ShoppingCart {
             alert("Your shopping cart is already empty");
             return;
         }
+
+        const isCartCleared = confirm(
+            "Are you sure you want to clear all items from your shopping cart?"
+        );
+
     }
 
     calculateTotal() {
