b15453afb5f9337e29af69d110c78cfe22bf2916
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-09-05 18:22:09 +0300

    step 51

--- script.js
+++ script.js
@@ -138,7 +138,9 @@ class ShoppingCart {
         const subTotal = this.items.reduce((total, item) => total + item.price, 0);
         const tax = this.calculateTaxes(subTotal);
         this.total = subTotal + tax;
-        cartSubTotal.textContent = `$${subTotal.toFixed(2)}`
+        cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+        cartTaxes.textContent = `$${tax.toFixed(2)}`;
+        cartTotal.textContent = `$${this.total.toFixed(2)}`;
     }
 
     calculateTaxes(amount) {
